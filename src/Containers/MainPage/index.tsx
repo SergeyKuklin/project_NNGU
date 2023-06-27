8a0b6be973f2f38ae6c91dcb33befab68c92c21b
KuklinSergey <sergey_kuk@outlook.com>
2023-06-27 23:08:02 +0300

    MainBanner

--- src/Containers/MainPage/index.tsx
+++ src/Containers/MainPage/index.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import styles from './styles.module.scss';
-import { Nav } from '../../Components/Nav';
 import { MainBanner } from './MainBanner';
 
 export const MainPage = () => {
