6341c13e5d4b65e229a547f05085b95228f815bc
KuklinSergey <sergey_kuk@outlook.com>
2023-06-28 22:31:27 +0300

    MainHostel_v1

--- src/Containers/MainPage/MainHostel/index.tsx
+++ src/Containers/MainPage/MainHostel/index.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import styles from './styles.module.scss';
+import { MainButton } from '../../../Components/MainButton';
 
 export const MainHostel = () => {
   return (
         <div className={styles.container}>
             <div className={styles.block}>
                 <div className={styles.block_1}>
+                    <MainButton />
                     <p>Хостел — бюджетная мини-гостиница, которая в первую очередь рассчитана на молодёжь. Главное отличие от остальных форматов: вам принадлежит только койко-место. Кухня, зона отдыха, ванная и туалеты — всё общее. Это самый экономный вариант для тех, кто не задерживается долго в одном месте.</p>
                 </div>
-                <div className={styles.block_2}>2</div>
-                <div className={styles.block_3}>3</div>
-                <div className={styles.block_4}>4</div>
+                <div className={styles.block_2}></div>
+                <div className={styles.block_3}>
+                    <div></div>
+                    <div>
+                        <p>Хостел один из самых лучших вариантов для путешействий</p>
+                    </div>
+                    <div>
+                        <p>Каждый вариант уникален и подчиняется своим правилам</p>
+                    </div>
+                    <div></div>
+                </div>
+                <div className={styles.block_4}>
+                    <div></div>
+                    <div></div>
+                </div>
             </div>
         </div>
   );
--- src/Containers/MainPage/index.tsx
+++ src/Containers/MainPage/index.tsx
@@ -2,15 +2,13 @@ import React from 'react';
 import { MainBanner } from './MainBanner';
 import { MainNN } from './MainNN';
 import { MainHostel } from './MainHostel';
-import { MainButton } from '../../Components/MainButton';
 
 export const MainPage = () => {
   return (
     <>
         {/* <MainBanner />
         <MainNN /> */}
-        {/* <MainHostel /> */}
-        <MainButton />
+        <MainHostel />
     </>
   )
 }
